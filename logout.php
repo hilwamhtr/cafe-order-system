0a795f70b4d304d976cdb01ad49cb25222533400
hilwamhtr <hilwamhtr@gmail.com>
2024-12-01 21:39:33 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,14 +1,8 @@
 <?php
-// Memulai sesi
 session_start();
-
-// Menghapus semua data sesi
 session_unset();
-
-// Mengakhiri sesi
 session_destroy();
 
-// Mengarahkan pengguna kembali ke halaman login
 header("Location: login.php");
 exit();
 ?>
